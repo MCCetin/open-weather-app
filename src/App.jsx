e4184128c16483860583769a57a8239a69b77d5a
MCCetin <mcan.cetin@mf.karaelmas.edu.tr>
2023-01-17 18:14:12 +0300

    other "components" are implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,36 @@
 import { useEffect, useState } from "react";
 import Dropdown from "./assets/Dropdown";
+import MainContext from "../context/MainContext";
+import Current from "./assets/Current/Current";
+import Forecast from "./assets/Forecast/Forecast";
+import "./main.css";
+
+const API_KEY = "f641699704974e889946cdddac0996ff";
+
 function App() {
-  const [data, setData] = useState({});
-  const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    getWeatherData();
-  }, []);
-  async function getWeatherData(cityID) {
-    try {
-      const response = await fetch(
-        "https://api.openweathermap.org/data/2.5/forecast?id=315808&units=metric&appid=f641699704974e889946cdddac0996ff"
-      );
-      const data = await response.json();
-      setData(
-        data.list.map((item) => {
-          const dateAndTime = item.dt_txt.split(" ");
-          return {
-            date: dateAndTime[0],
-            time: dateAndTime[1],
-            temperature: Math.round(item.main.temp),
-            weather: item.weather[0].main,
-            wind: item.wind.speed,
-            humidity: item.main.humidity,
-            icon: item.weather[0].icon,
-          };
-        })
-      );
-    } catch (error) {
-      console.error(error);
-    }
-    setIsLoading(false);
-  }
+  const [forecast, setForecast] = useState([]);
+  const [current, setCurrent] = useState({});
+  const [cityID, setCityID] = useState("745042");
 
   return (
     <div className="App">
-      <Dropdown />
-      <br />
-      {isLoading ? (
-        <p>loading...</p>
-      ) : (
-        <div>
-          <img
-            src={`http://openweathermap.org/img/wn/${data[0].icon}@2x.png`}
-            alt=""
-          />
-          <p>{data[0].temperature}</p>
+      <MainContext.Provider
+        value={{
+          API_KEY,
+          cityID,
+          current,
+          forecast,
+          setCityID,
+          setCurrent,
+          setForecast,
+        }}
+      >
+        <div className="info">
+          <Dropdown />
+          <Current />
         </div>
-      )}
+        <Forecast />
+      </MainContext.Provider>
     </div>
   );
 }
